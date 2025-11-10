660652b9bf10da1894e51ed347aa9bc6b6497208
VKoparov <valentinkoparov@gmail.com>
2025-11-10 11:42:18 +0200

    enhance(config): updated lsp

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -78,36 +78,22 @@ return {
   -- add tsserver and setup with typescript.nvim instead of lspconfig
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-        tsserver = {},
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
+    config = function()
+      -- Define ts_ls configuration (Neovim >= 0.10)
+      vim.lsp.config("ts_ls", {
+        flags = { debounce_text_changes = 300 },
+        -- Add anything you previously had in opts.server, e.g.:
+        on_attach = function(client, bufnr)
+          -- Example keymaps (optional)
+          local map = vim.keymap.set
+          map("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = bufnr, desc = "Organize Imports" })
+          map("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { buffer = bufnr, desc = "Rename File" })
         end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
+      })
+
+      -- Enable the server
+      vim.lsp.enable({ "ts_ls" })
+    end,
   },
 
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
